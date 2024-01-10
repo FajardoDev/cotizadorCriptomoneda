e8cd31f213d7065bd4eeb37a8c047b0443ded9ba
ariel-dev@FajardoDev <arielfajardo1991@gmail.com>
2024-01-09 20:44:52 -0400

    archivo actualizando cotizador criptos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from "react";
+import styled from "@emotion/styled";
+import ImagenCripto from "./img/imagen-criptos.png";
 
-import viteLogo from "/vite.svg";
+const Contenedor = styled.div`
+	max-width: 900px;
+	margin: 0 auto;
+	width: 90%;
+
+	@media (min-width: 992px) {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		column-gap: 2rem;
+	}
+`;
+
+const Imagen = styled.img`
+	max-width: 400px;
+	width: 80%;
+	margin: 100px auto 0 auto;
+	display: block;
+`;
+
+const Heading = styled.h1`
+	font-family: "Lato", sans-serif;
+`;
 
 function App() {
 	return (
-		<>
-			<h1 className="text-blue-600 text-center my-6 font-extrabold">
-				Cotizador de Criptomonedas
-			</h1>
-		</>
+		<Contenedor>
+			<Imagen src={ImagenCripto} alt="Imagen Criptomonedas" />
+
+			<Heading className=" font-extrabold">Cotizador de Criptomonedas</Heading>
+		</Contenedor>
 	);
 }
 
 export default App;
+
+//  const Heading = styled.h1 `
+// font-size: 2rem;
+// font-family: lato;
+// `;
+
+// <Heading>Nuestros Productos</Heading>
